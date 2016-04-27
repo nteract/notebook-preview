0cc9c31161ab3bff37e0c30072b09203b6122162
Kyle Kelley <rgbkrk@gmail.com>
2016-04-26 21:50:00 -0500

    Prepare the codemirror hackery

---
+++ test/setup.js
@@ -0,0 +1,29 @@
+// From https://github.com/airbnb/enzyme/blob/master/docs/guides/jsdom.md
+
+var jsdom = require('jsdom').jsdom;
+
+var exposedProperties = ['window', 'navigator', 'document'];
+
+global.document = jsdom('');
+global.window = document.defaultView;
+Object.keys(document.defaultView).forEach((property) => {
+  if (typeof global[property] === 'undefined') {
+    exposedProperties.push(property);
+    global[property] = document.defaultView[property];
+  }
+});
+
+global.navigator = {
+  userAgent: 'node.js'
+};
+
+// HACK: Polyfil that allows codemirror to render in a JSDOM env.
+global.window.document.createRange = function createRange() {
+  return {
+    setEnd: () => {},
+    setStart: () => {},
+    getBoundingClientRect: () => {
+      return { right: 0 };
+    }
+  }
+};
