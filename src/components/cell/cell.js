03000e26a5efe2daa87643e620161fc95b40903f
Kyle Kelley <rgbkrk@gmail.com>
2016-04-26 20:54:47 -0500

    getting cellular

---
+++ src/components/cell/cell.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import CodeCell from './code-cell';
+import MarkdownCell from './markdown-cell';
+
+const Cell = (props) => {
+  const cell = props.cell;
+  const type = cell.get('cell_type');
+  return (
+    <div
+      className="cell"
+    >
+      {
+      type === 'markdown' ?
+        <MarkdownCell {...props} /> :
+        <CodeCell {...props} />
+      }
+    </div>
+  );
+};
+
+Cell.propTypes = {
+  cell: React.PropTypes.any,
+  id: React.PropTypes.string,
+  focusedCell: React.PropTypes.string,
+  onCellChange: React.PropTypes.func,
+  running: React.PropTypes.bool,
+};
+
+export default Cell;
