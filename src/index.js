76e6bb9575bb4c799330951a11caec374b52334c
Kyle Kelley <rgbkrk@gmail.com>
2016-05-02 12:18:19 -0500

    Use Tom's gists

--- src/index.js
+++ src/index.js
@@ -6,9 +6,15 @@ import NotebookPreview from 'notebook-preview';
 const includes = require('lodash.includes');
 
 const gistIDs = [
-  '0787d2fd8b898368503bd9469b50383e',
-  '35fdfae490529b47e7ea2c44c144c593',
-  '1ba9b5d825af9e349bc3',
+  '53f2d7bbc69936bd7a4131c0890fc61d',
+  'ee778e32b8e62cf634929abe229a8555',
+  '7eadc20426451a0604e26e6f084cac02',
+  '0a9389389ec5ff303c5d5fbfa6bea021',
+  'b71d96c48326a0e05904a5ad4a96d2b5',
+  '93239f6b97237abf117a348a56afc9e2',
+  // '0787d2fd8b898368503bd9469b50383e',
+  // '35fdfae490529b47e7ea2c44c144c593',
+  // '1ba9b5d825af9e349bc3',
   // 'd8ac62bc0726ecdc0f22', // FIXME: notebook has HTML inside Markdown
   // 'b057942206c4e3d98153', // FIXME: notebook has inline maths
 ];
