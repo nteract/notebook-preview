4794fce8a7936f292666be20ff74718e8124e8a2
Kyle Kelley <rgbkrk@gmail.com>
2016-06-11 12:44:19 -0600

    Set up a demo page, from notebook-preview-demo

---
+++ demo/src/fetchers.js
@@ -0,0 +1,22 @@
+const includes = require('lodash.includes');
+
+export function fetchFromGist(gistId) {
+  var path = `https://api.github.com/gists/${gistId}`;
+  return fetch(path)
+    .then((data) => data.json())
+    .then((ghResponse) => {
+      for (var file in ghResponse.files) {
+        if (includes(file, '.ipynb')) {
+          const fileResponse = ghResponse.files[file];
+          if (fileResponse.truncated) {
+            return fetch(fileResponse.raw_url)
+                    .then((resp) => resp.json())
+          }
+
+          const nbString = fileResponse.content;
+          const nbJSON = JSON.parse(nbString);
+          return nbJSON;
+        }
+      }
+    })
+}
